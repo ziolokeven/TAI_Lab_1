891de218f43fb12591aa807fc883777cd14deef6
ziolokeven <ziolokeven@gmail.com>
2020-03-15 15:10:39 +0100

    dodanie skryptu

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href="index.html">Home </a>'+
+	  '<a href="contact.html">Kontakt </a>'+
+	  '<a href="quiz.html">Quiz </a></nav>';
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
